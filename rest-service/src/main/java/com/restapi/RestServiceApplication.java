18fdfac96889fb9c57eb0d29e7caac3247f64649
tyagi.pallav1601@gmail.com <tyagi.pallav1601@gmail.com>
2016-09-25 14:27:28 +0530

    depedency check

--- rest-service/src/main/java/com/restapi/RestServiceApplication.java
+++ rest-service/src/main/java/com/restapi/RestServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableDiscoveryClient
 @SpringBootApplication
